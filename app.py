c07db7e7d86ea882bfbdc50cc05b03bc181ea2ce
zagnouneotmane <zagnoune.otmane@gmail.com>
2024-01-08 22:21:09 +0100

    display chat history

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI, HuggingFaceHub
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.embeddings import HuggingFaceEmbeddings, HuggingFaceInstructEmbeddings
 from dotenv import load_dotenv
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import HuggingFaceHub
+from htmlTemplates import css, user_template, bot_template
+
+
 
 def get_pdf_text(pdf_documents):
   text = ""
@@ -44,9 +48,14 @@ def main():
   load_dotenv()
   st.set_page_config(page_title='Chat Multiple PDFs',page_icon=':books:')
   
-  
+  if "conversation" not in st.session_state:
+    st.session_state.conversation = None
+  if "history" not in st.session_state:
+    st.session_state.history = []
+
   with st.sidebar:
-    pdf_documents = st.file_uploader('Upload your PDF file', type=['pdf'], accept_multiple_files=True)
+    st.subheader('Your documents')
+    pdf_documents = st.file_uploader('Upload your PDF files and click on "Process"', type=['pdf'], accept_multiple_files=True)
   
     if st.button('Process'):
       with st.spinner('Processing...'):
@@ -55,6 +64,8 @@ def main():
         text_chunks = get_chunks(pdf_text)
 
         vectorstore = get_vectorstore(text_chunks)
+        
+        # llm = OpenAI()
         llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
         memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
         st.session_state.conversation = ConversationalRetrievalChain.from_llm(
@@ -68,11 +79,20 @@ def main():
 
   st.title('Chat with Multiple PDFs :books:')
   user_question = st.text_input('Ask a question about the PDFs')
+  st.markdown(css, unsafe_allow_html=True)
   if user_question is not None and user_question != '':
     with st.spinner('Searching...'):
       response = st.session_state.conversation({'question': user_question})
-      st.write(response)
-  else:
+      st.session_state.history = response['chat_history']
+
+      for i, message in enumerate(st.session_state.history):
+        if i % 2 == 0:
+           st.write(user_template.replace("$MSG", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("$MSG", message.content), unsafe_allow_html=True)
+
+
+  if st.session_state.history is None or len(st.session_state.history) == 0:
     st.write('Waiting for your question...')
 
 if __name__ == '__main__':
